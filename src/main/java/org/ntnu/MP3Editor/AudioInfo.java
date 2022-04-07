83ae6e86f029e4ad5ecfee8be8d41af07b153f69
b06608062 <65601675+b06608062@users.noreply.github.com>
2022-04-07 11:04:14 +0800

    Update AudioInfo.java

--- src/main/java/org/ntnu/MP3Editor/AudioInfo.java
+++ src/main/java/org/ntnu/MP3Editor/AudioInfo.java
@@ -16,17 +16,17 @@ public class AudioInfo {
 				System.out.println("---- Information ----");
 				for (Object objectName : m.getMetadata().keySet()) {
 					System.out.println(objectName.toString().substring(0,1).toUpperCase() + objectName.toString().substring(1).toLowerCase() + ": " + m.getMetadata().get(objectName).toString());
-                }
+                		}
 				System.out.println("Duration: " + m.getDuration() / 60000 + " m:" + (m.getDuration() / 1000) % 60 + " s");
 				System.out.println("Format: " + m.getFormat());
 				System.out.println("Bitrate: " + m.getAudio().getBitRate() / 1000 + "kbps");
 				System.out.println("Sampling rate: " + (double)m.getAudio().getSamplingRate() / 1000 + "kHz");
 				System.out.println("Channels: " + m.getAudio().getChannels());
 				System.out.println("Decoder: " + m.getAudio().getDecoder());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+            		} catch (Exception e) {
+                		e.printStackTrace();
+            		}
+        	}
 		
 	}
 }
